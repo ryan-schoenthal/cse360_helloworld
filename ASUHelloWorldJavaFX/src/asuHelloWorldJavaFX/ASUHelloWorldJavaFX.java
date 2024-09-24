102e950a179cb9561b3fdf15a54d1ccaf682bfe9
RyanSchoenthal <jschoent@asu.edu>
2024-09-24 15:41:22 -0700

    Changed the button text and title

--- ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
+++ ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
@@ -16,9 +16,9 @@ public class ASUHelloWorldJavaFX extends Application {
     public void start(Stage primaryStage) {
     	System.out.println("ASU Hello World!");
     	System.out.println("It started!");
-        primaryStage.setTitle("Ryan Schoenthal Hello World Spring 2024");
+        primaryStage.setTitle("In class GitHub activity");
         Button btn = new Button();
-        btn.setText("Display: 'Ryan says: Hello World!'");
+        btn.setText("Display: Ryan's second commit");
         btn.setOnAction(new EventHandler<>() {
             public void handle(ActionEvent event) {
                 System.out.println("Ryan says: Hello World!");
